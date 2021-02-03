f3cdf944797b6559fff94c216e00b02b02d66ac8
Deepthishreebhat <62230022+Deepthishreebhat@users.noreply.github.com>
2021-02-03 13:07:03 +0530

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,19 @@
+echo "******************************"
+echo "Initializing...."
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "  Matrix.......OK"
+sleep 5
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "  Sequels......Not so good"
+echo "**"
+echo "Hello from an image built using a secure build server."
